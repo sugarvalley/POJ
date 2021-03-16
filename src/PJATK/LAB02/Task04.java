b34b251ed25f458624e141a6f2397326944d0097
sugarvalley <cqr@o2.pl>
2021-03-16 15:44:12 +0100

    Task05 done

--- src/PJATK/LAB02/Task04.java
+++ src/PJATK/LAB02/Task04.java
@@ -3,12 +3,13 @@ package PJATK.LAB02;
 
 public class Task04 {
     public static void main(String[] args) {
-        int[][] array = {{1,2,3,4,5},{0,0,0,0,0},{6,7,8,9,1}};
+        int[][] array = {{1, 2, 3, 4, 5}, {0, 0, 0, 0, 0}, {6, 7, 8, 9, 1}};
         show_array(array);
     }
+
     public static void show_array(int[][] array) {
-        for (int i=0; i<array.length; i++) {
-            for (int j=0; j<array[i].length; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println();
