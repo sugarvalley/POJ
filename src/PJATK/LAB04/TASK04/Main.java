35acbd35863a8cddc7cc0a3b3ca6ac6307f8360b
sugarvalley <cqr@o2.pl>
2021-03-29 09:29:57 +0200

    Task04 done

--- src/PJATK/LAB04/TASK04/Main.java
+++
@@ -1,7 +0,0 @@
-package PJATK.LAB04.TASK04;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/PJATK/LAB04/TASK04/Simulation.java
+++ src/PJATK/LAB04/TASK04/Simulation.java
@@ -1,5 +1,20 @@
 package PJATK.LAB04.TASK04;
 
-public class Simulation {
+import java.util.Scanner;
 
+public class Simulation {
+    public static void main(String[] args) {
+        System.out.println("Insert number of tries for simulation: ");
+        Scanner scanner = new Scanner(System.in);
+        int numTries = scanner.nextInt();
+        int numHeads = 0;
+        Coin coin = new Coin();
+        for (int i=0; i<numTries; i++) {
+            coin.tossCoin();
+            if (coin.isHeads()) {
+                numHeads++;
+            }
+        }
+        System.out.println((100 * numHeads)/(double)numTries + "% of heads | "+ (100*(numTries-numHeads))/(double)numTries + "% of tails | after " + numTries + " tosses");
+    }
 }
